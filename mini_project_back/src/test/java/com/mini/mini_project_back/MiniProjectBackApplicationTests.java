f86a72d24e13f59d2e32908ab1c974fb0421ebe5
kohyeokjae <kohj0686@naver.com>
2025-08-07 12:51:55 +0900

    feat: mini project start

---
+++ mini_project_back/src/test/java/com/mini/mini_project_back/MiniProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.mini_project_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
